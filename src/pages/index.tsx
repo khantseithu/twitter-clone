22d5ffb5ddb437315e4b528e7e601cf103698527
Khant Sithu <khantseithuu@gmail.com>
2023-05-16 04:36:34 +0000

    implement create tweet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { type NextPage } from "next";
-import NewTweerForm from "~/components/NewTweetForm";
+import { NewTweetForm } from "~/components/NewTweetForm";
 
 
 const Home: NextPage = () => {
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
     <header className="sticky top-0 z-10 flex justify-between items-center p-4 border-b">
       <h1 className="font-bold">Home</h1>
       </header>  
-      <NewTweerForm />
+      <NewTweetForm />
     </>
   );
 };
